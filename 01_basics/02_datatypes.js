dc93b80b77b7a730b64603ff07508f9e7d967600
Amardeep Phule <35063838+amarphule@users.noreply.github.com>
2023-10-18 10:03:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,11 @@
+// primitive datatypes
+// string       =>"", "Amar"
+// number       => 18 (value between 2 to power of 53)
+// bigInt        => 10n
+// boolean      => true
+// null         => object standalone value
+// undefined    => undefined
+// symbol        => unique
+
+let state = null; 
+console.log(typeof state);
