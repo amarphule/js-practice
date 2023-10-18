30b350c6c02869974fb17594a3261bf1e373a311
Amardeep Phule <35063838+amarphule@users.noreply.github.com>
2023-10-18 11:39:06 +0000

    conversion datatype

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,24 @@
+// let year = 1991
+// console.log(typeof year, year);
+
+// let yearInString = String(year)
+// console.log(typeof yearInString, yearInString); 
+
+
+// let age = "18abc"
+// let numberAge = Number(age)
+// console.log(typeof numberAge, numberAge);
+
+
+let isLoggedIn = "";
+console.log(typeof isLoggedIn, isLoggedIn);
+
+let isLoggedInNumber = Boolean(isLoggedIn)
+console.log(typeof isLoggedInNumber, isLoggedInNumber);
+
+/**
+ * true => 1 ; false => 0               ( Boolean to Number)
+ * ""=> 0; "Amar"=> 1; "10abc" => NaN   ( string to Number())
+ * "10abc" => true ; "" => false        ( string to Boolean())
+ * null=> 0; undefined=> NaN            ( to Number())
+ */
