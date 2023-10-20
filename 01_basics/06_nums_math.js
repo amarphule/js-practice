0aa4e1ced4d8122954fee9e73f2b71d3269ba7a3
Amardeep Phule <35063838+amarphule@users.noreply.github.com>
2023-10-20 08:37:03 +0000

    numbers and math

---
+++ 01_basics/06_nums_math.js
@@ -0,0 +1,18 @@
+const percent = 100
+const actualPercent = new Number(100)
+
+// console.log(percent);  // define number type at run time
+// console.log( actualPercent); // guarented number type
+
+let decimalNumber = 100.39344
+console.log(decimalNumber.toFixed(2));
+
+let readableNumber = 1000000.
+console.log(readableNumber.toLocaleString());
+console.log(readableNumber.toLocaleString('en-IN'));
+
+// define Range
+// random value between 10 and 20
+const min = 10
+const max = 20
+console.log(Math.floor((Math.random()* (max-min+1))+min));
