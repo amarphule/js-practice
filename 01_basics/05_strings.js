b5c55fe05ccd16bfd851c1fec82dff1fdc199763
Amardeep Phule <35063838+amarphule@users.noreply.github.com>
2023-10-20 06:48:26 +0000

    string methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const fName = "Amardeep";
+const lName = "Phule"
+
+// concat String using +
+console.log("Fullname is "+ fName + " " + lName);
+
+// concat string using templete literal
+console.log(`Fullname is ${fName} ${lName}`);
+
+/**
+ * Methos to manupulate string
+ * 
+ * .toUpperCase()
+ * .toLowerCase()
+ * .substring().
+ * .slice()
+ * .splice()
+ * .replace()
+ * .split()
+ * .join()
+ * .reverse()
+ * .charAt()
+ * .indexOf()
+ * 
+ */
