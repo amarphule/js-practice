3f837b0d6e9862cdcf730031c4bb96838e1454a8
Amardeep Phule <35063838+amarphule@users.noreply.github.com>
2023-11-02 05:07:30 +0000

    function declaration and calling

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+// function declaration
+function welcome(){
+    console.log("Hello world!");
+}
+
+// function call
+welcome();
+
+// function (expression) declaration
+// treat as variable till so can't call before declaration
+const welcome1 = function (){
+    console.log("Hello world!");
+}
+
+// function call
+welcome1();
