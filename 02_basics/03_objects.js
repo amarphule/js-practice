236b1b45c46ad634d702c8212d98c2feed695a17
Amardeep Phule <35063838+amarphule@users.noreply.github.com>
2023-11-02 05:01:28 +0000

    access object property

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,17 @@
+// singleton
+// created with constructor
+
+// object literal
+
+const mySymbol = Symbol("key1")
+
+const user = {
+    name: "Amardeep",
+    age: 18,
+    'full name': "Amardeep Phule",
+
+}
+
+console.log(user.name);
+console.log(user['age']);
+console.log(user['full name']);
